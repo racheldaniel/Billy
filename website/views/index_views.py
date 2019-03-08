dd76da03fb557c88a3c64f37e535761cd631d343
Rachel Daniel <rachdaniel88@gmail.com>
2019-03-08 10:15:34 -0600

    installed requests, successful fetch

--- website/views/index_views.py
+++ website/views/index_views.py
@@ -1,11 +1,22 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
-from django.template import RequestContext
+from django.db import connection
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+import requests
+from website.models import *
 
 # from website.forms import ProductForm
 # from website.models import Product, ProductType, RecommendedProduct
 
 def index(request):
-    context={}
-    template_name = 'index.html'
-    return render(request, template_name, context)
+    if request.method == "GET":
+        url = 'https://api.propublica.org/congress/v1/115/both/bills/active.json'
+        headers = {'X-API-Key': 'S6kqwpSxckFECU8hUHE3obzUfli28WzAOrNs1Qrh'}
+        r = requests.get(url, headers=headers).json()
+        context={}
+        template_name = 'index.html'
+        return render(request, template_name, context)
+
