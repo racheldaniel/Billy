cc7cab4746053d836ab4e4747de23a6db1593cdb
Rachel Daniel <rachdaniel88@gmail.com>
2019-03-08 11:13:24 -0600

    user can now view bill details

--- website/views/index_views.py
+++ website/views/index_views.py
@@ -13,7 +13,7 @@ from website.models import *
 
 def index(request):
     if request.method == "GET":
-        url = 'https://api.propublica.org/congress/v1/115/both/bills/active.json'
+        url = 'https://api.propublica.org/congress/v1/115/both/bills/introduced.json'
         headers = {'X-API-Key': 'S6kqwpSxckFECU8hUHE3obzUfli28WzAOrNs1Qrh'}
         r = requests.get(url, headers=headers).json()
         bills = r["results"][0]["bills"]
@@ -21,3 +21,17 @@ def index(request):
         template_name = 'index.html'
         return render(request, template_name, context)
 
+
+def bill_details(request, bill_slug):
+    """This function loads a specific bill's detail page.
+
+    Parameters -- bill_slug: on proPublica api, bill_slug is the bill_id without the congress number
+    """
+    if request.method == "GET":
+            url = f'https://api.propublica.org/congress/v1/115/bills/{bill_slug}.json'
+            headers = {'X-API-Key': 'S6kqwpSxckFECU8hUHE3obzUfli28WzAOrNs1Qrh'}
+            r = requests.get(url, headers=headers).json()
+            bill = r["results"][0]
+            context={"bill": bill}
+            template_name = 'bill_details.html'
+            return render(request, template_name, context)
